e11d7f4087e0e4eaee6c494dae1209e06396efd4
Erick Rosa <erickpmotta@gmail.com>
2021-10-22 00:15:40 -0300

    fixing carrousel issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,10 @@
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
+    items:1,
+    loop: true,
+    margin:10,
+    nav: true,
+ 
+    
 })
 
 
